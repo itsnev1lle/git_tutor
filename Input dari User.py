47302458b523f12c40b5dab7b15205b7d06e5553
Yuchiro Paliyama <reenhard07@gmail.com>
2024-03-19 00:18:19 +0900

    First Commit

---
+++ Input dari User.py
@@ -0,0 +1,7 @@
+# input
+
+data = input("Masukkan data : ")
+print("data =",data,"type =", type(data))
+
+data_int = int(input("Masukkan angka :"))
+print("data =",data_int,"type =", type(data_int))
